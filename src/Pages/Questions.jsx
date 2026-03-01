65e3f0d3cb00e5813a23f1c300dbdc41046fe74d
shahhiloni <shahhiloni411@gmail.com>
2026-03-01 19:24:45 +0530

    01-03-2026

--- src/Pages/Questions.jsx
+++ src/Pages/Questions.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
-import  HomeData from '../JSON/HomeData.json';
+import  HomeData  from '../JSON/HomeData.json';
 
 const Questions = () => {
     const {id} = useParams();
