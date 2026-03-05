0a72525b5f3ca3a46884c974247a150eacb3b816
shahhiloni <shahhiloni411@gmail.com>
2026-03-05 16:01:10 +0530

    05-03-2026

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,23 +1,43 @@
+import React from "react";
 import HomeData from "../JSON/HomeData.json";
-import React from 'react'
 import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
+
   return (
-    <div>
-  <h3> ALL Courses </h3>    
-  {HomeData.Courses.map((course) => (
-    <div key={course.CourseId}
-    onClick = {() => navigate(`/questions/${course.CourseId}`)}
-    style={{ border: "2px solid black", padding: "20px", margin: "20px", Course: "pointer"}}>
-<img src={course.image} />
+    <div className="container">
+      <h3 className="text-center my-3">All Courses</h3>
 
-  <h3>{course.CourseName} </h3>
-    </div>
-      ))}
+      <div className="row">
+        {HomeData.Courses.map((course) => (
+          <div className="col-md-4 mb-3" key={course.CourseId}>
+            <div className="card text-center">
+              
+              <img
+                src={course.image}
+                className="card-img-top mx-auto mt-2"
+                alt={course.CourseName}
+                style={{ height: "100px", width: "100px" }}
+              />
+
+              <div className="card-body">
+                <h5 className="card-title">{course.CourseName}</h5>
+
+                <button
+                  className="btn btn-primary"
+                  onClick={() => navigate(`/questions/${course.CourseId}`)}
+                >
+                  View Questions
+                </button>
+
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
-  )
-}
+    </div>
+  );
+};
 
-export default Home
+export default Home;
