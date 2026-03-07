48de0c59f6acf0fac953277681e621fbc365d966
shahhiloni <shahhiloni411@gmail.com>
2026-03-07 17:42:58 +0530

    07-03-2026

--- src/App.js
+++ src/App.js
@@ -4,25 +4,33 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 // Authentication
 import Register from "./Authentication/Register";
 import Login from "./Authentication/Login";
+import ForgetPassword from "./Authentication/ForgetPassword";
 
 //not Found 
 import NotFound from './Components/NotFound';
 
 // page
 import Home from "./Pages/Home";
+import Feedback from "./Pages/Feedback";
+import Contact from "./Pages/Contact";
 import Questions from './Pages/Questions';
+
 const App = () => {
   return (
     <BrowserRouter>
     <Routes>
+      {/* // Authentication */}
       <Route path="/Register" element={<Register />} />
       <Route path="/login" element={<Login />} />
+      <Route path ="/forget-password" element={<ForgetPassword />} />
 
       {/* // Pages  */}
       <Route path="/" element={<Home/>} />
+      <Route path="/feedback" element={<Feedback/>} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/" element={<Home/>} />
       <Route path="/questions/:id" element={<Questions />}/>
     
-
       {/* Url not found */}
       <Route path="*" element={<NotFound />} />
       
